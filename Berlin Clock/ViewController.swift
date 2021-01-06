1b736a04ec531599014e800fe20a5a6acd5c82ef
George Muntean <george.muntean@icloud.com>
2021-01-06 17:03:00 +0200

    Added base project

---
+++ Berlin Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Berlin Clock
+//
+//  Created by George Muntean on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
