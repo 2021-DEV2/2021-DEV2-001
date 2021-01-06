db0a4302f98c9c23683d94f07c5cb9b7154235a7
Dev2 <Dev2@dev.com>
2021-01-06 20:03:40 +0200

    added the seconds functionality for the Berlin Clock Service

---
+++ Berlin Clock/Services/BerlinClockService.swift
@@ -0,0 +1,37 @@
+//
+//  BerlinClockService.swift
+//  Berlin Clock
+//
+//  Created by George Muntean on 06/01/2021.
+//
+
+import Foundation
+
+/**
+ BerlinClockService will help processing a given time from Swift **Date** format to a Berlin time
+*/
+
+class BerlinClockService {
+    
+    // Each item from the time format represents the number of illuminated lamps from it's group
+    // In the UI, the illuminated lamps will be displayed first
+    struct TimeFormat {
+        typealias NumberOfIlluminatedLamps = Int
+        
+        var seconds: NumberOfIlluminatedLamps = 0
+        var fiveHour: NumberOfIlluminatedLamps = 0
+        var oneHour: NumberOfIlluminatedLamps = 0
+        var fiveMinute: NumberOfIlluminatedLamps = 0
+        var oneMinute: NumberOfIlluminatedLamps = 0
+    }
+    
+    func convert(time: Date) -> TimeFormat {
+        var timeFormat = TimeFormat()
+        let calendar = Calendar.current
+        let seconds = calendar.component(.second, from: time)
+        
+        // When the number of seconds is odd, the lamp should be turned on
+        timeFormat.seconds = 1 - (seconds % 2)
+        return timeFormat
+    }
+}
--- Berlin ClockTests/BerlinClockServiceTests.swift
+++ Berlin ClockTests/BerlinClockServiceTests.swift
@@ -1,10 +1,11 @@
 //
-//  Berlin_ClockTests.swift
-//  Berlin ClockTests
+//  BerlinClockServiceTests.swift
+//  BerlinClockServiceTests
 //
 //  Created by George Muntean on 06/01/2021.
 //
-import UIKit
+
+import Foundation
 import XCTest
 @testable import Berlin_Clock
 
@@ -21,12 +22,12 @@ class BerlinClockServiceTests: XCTestCase {
     }
 
     func testSecondsLamp() {
-        var time = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())
+        var time = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())!
         var berlinTime = berlinClockService.convert(time: time)
-        XCTAssertEqual(berlinTime.secondsLamp, 1, "Seconds lamp should be illuminated for even seconds")
+        XCTAssertEqual(berlinTime.seconds, 1, "Seconds lamp should be illuminated for even seconds")
         
-        time = Calendar.current.date(bySettingHour: 23, minute: 59, second: 59, of: Date())
-        var berlinTime = berlinClockService.convert(time: time)
-        XCTAssertEqual(berlinTime.secondsLamp, 0, "Seconds lamp should be off for odd seconds")
+        time = Calendar.current.date(bySettingHour: 23, minute: 59, second: 59, of: Date())!
+        berlinTime = berlinClockService.convert(time: time)
+        XCTAssertEqual(berlinTime.seconds, 0, "Seconds lamp should be off for odd seconds")
     }
 }
