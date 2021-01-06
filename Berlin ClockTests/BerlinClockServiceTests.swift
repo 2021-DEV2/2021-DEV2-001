0bdc5e49c53b07a4511d8ca3416d3588479f3c46
Dev2 <Dev2@dev.com>
2021-01-06 20:17:34 +0200

    added test for five hour lamps

--- Berlin ClockTests/BerlinClockServiceTests.swift
+++ Berlin ClockTests/BerlinClockServiceTests.swift
@@ -30,4 +30,22 @@ class BerlinClockServiceTests: XCTestCase {
         berlinTime = berlinClockService.convert(time: time)
         XCTAssertEqual(berlinTime.seconds, 0, "Seconds lamp should be off for odd seconds")
     }
+    
+    func testFiveHoursLamps() {
+        var time = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())!
+        var berlinTime = berlinClockService.convert(time: time)
+        XCTAssertEqual(berlinTime.fiveHour, 0, "Five hour lamps should be all off at midnight")
+        
+        time = Calendar.current.date(bySettingHour: 23, minute: 59, second: 59, of: Date())!
+        berlinTime = berlinClockService.convert(time: time)
+        XCTAssertEqual(berlinTime.fiveHour, 4, "Five hour lamps should be all on one minute before midnight")
+        
+        time = Calendar.current.date(bySettingHour: 2, minute: 4, second: 0, of: Date())!
+        berlinTime = berlinClockService.convert(time: time)
+        XCTAssertEqual(berlinTime.fiveHour, 0, "Five hour lamps should be all off before five'o'clock")
+        
+        time = Calendar.current.date(bySettingHour: 16, minute: 35, second: 0, of: Date())!
+        berlinTime = berlinClockService.convert(time: time)
+        XCTAssertEqual(berlinTime.fiveHour, 3, "Five hour lamps should light 3 items after 15:00")
+    }
 }
